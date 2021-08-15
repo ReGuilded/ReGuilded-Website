868412c190b2bb0b25dbaa82c9d64c3f2c28c642
Nathaniel M <nmoratto629@gmail.com>
2021-08-15 15:07:58 -0700

    Dynamically creates downloads list items now.

--- ReGuilded/Pages/Downloads.cshtml.cs
+++ ReGuilded/Pages/Downloads.cshtml.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using ReGuilded.Pages.Util;
 
-namespace ReGuilded.Pages
-{
-    public class DownloadsModel : PageModel
-    {
+namespace ReGuilded.Pages {
+    public class DownloadsModel : PageModel {
         private readonly ILogger<DownloadsModel> _logger;
+        private readonly IMemoryCache _cache;
 
-        public DownloadsModel(ILogger<DownloadsModel> logger)
-        {
+        public DownloadsModel(ILogger<DownloadsModel> logger, IMemoryCache cache) {
             _logger = logger;
+            _cache = cache;
         }
 
-        public void OnGet()
-        {
+        public bool InternalServerError { get; private set; } = false;
+        public List<FetchedDownload> FetchedDownloads { get; set; }
+        public async Task OnGetAsync() {
+            try {
+                FetchedDownloads = await GithubUtil.FetchedDownloads(_cache, "github_downloads", TimeSpan.FromMinutes(30));
+            } catch (Exception e) {
+                InternalServerError = true;
+                Console.WriteLine("Internal Server Error:", e);
+            }
+            
         }
     }
 }
--- ReGuilded/Pages/Util/GithubUtil.cs
+++ ReGuilded/Pages/Util/GithubUtil.cs
@@ -1,3 +1,56 @@
-﻿namespace ReGuilded.Pages.Util {
-    public class GithubUtil { }
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using Octokit;
+
+namespace ReGuilded.Pages.Util {
+    public class GithubUtil {
+
+        private static readonly Regex WindowsRegex = new Regex($"exe");
+        private static readonly Regex LinuxRegex = new Regex($"AppImage");
+        private static readonly Regex MacRegex = new Regex($"dmg");
+        
+        public static async Task<List<FetchedDownload>> FetchedDownloads(IMemoryCache cache, string cacheKey, TimeSpan cacheTime) {
+            List<FetchedDownload> downloads;
+
+            if (!cache.TryGetValue(cacheKey, out downloads)) {
+                downloads = new List<FetchedDownload>();
+                GitHubClient githubClient = new(new ProductHeaderValue("ReGuilded-Website"));
+
+                var latestRelease = await githubClient.Repository.Release.GetLatest("ReGuilded", "ReGuilded-Setup");
+
+                foreach (var releaseAsset in latestRelease.Assets) {
+                    var downloadUrl = releaseAsset.BrowserDownloadUrl;
+                    var platform = WindowsRegex.Match(downloadUrl).Success ? "windows" 
+                                    : LinuxRegex.Match(downloadUrl).Success ? "linux" 
+                                    : MacRegex.Match(downloadUrl).Success ? "mac"
+                                    : null;
+
+                    if (platform != null) {
+                        downloads.Add(new FetchedDownload() {
+                            Platform = platform,
+                            DownloadUrl = downloadUrl,
+                            DisplayName = (char.ToUpper(platform[0]) + platform.Substring(1))
+                        });
+                    }
+                }
+
+                cache.Set(cacheKey, downloads, new MemoryCacheEntryOptions().AddExpirationToken(new CancellationChangeToken(new CancellationTokenSource(cacheTime).Token)));
+            }
+
+            return downloads;
+        }
+        
+    }
+
+    public class FetchedDownload {
+        public string Platform { get; set; }
+        public string DownloadUrl { get; set; }
+        public string DisplayName { get; set; }
+    }
 }
