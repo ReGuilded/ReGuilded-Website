a4c720e4c0287fd606015e483136800405bf0a07
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2022-01-25 21:37:02 +0200

    Add JSON transformation to Gulp

--- ReGuilded/gulpfile.js
+++ ReGuilded/gulpfile.js
@@ -1,14 +1,25 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const gulp = require("gulp"),
+    sass = require("gulp-sass")(require("sass")),
+    through2 = require("through2");
 
-gulp.task('default', async cb => compileSass())
-gulp.task('live-rebuild', async cb => {
-    gulp.watch(`./wwwroot/sass/**/*.sass`, compileSass)
-})
+gulp.task("default", compileSass);
+gulp.task("json", () => gulp.src(`./wwwroot/contributors.json`).pipe(transformJson).pipe(gulp.dest(`./wwwroot/`)));
+gulp.task("live-rebuild", async () => {
+    gulp.watch(`./wwwroot/sass/**/*.sass`, compileSass);
+});
 
 async function compileSass() {
     // Compile all SASS files
     gulp.src(`./wwwroot/sass/**/*.sass`)
-        .pipe(sass({ indentedSyntax: false, outputStyle: 'compressed' }))
-        .pipe(gulp.dest(`./wwwroot/css`))
+        .pipe(sass({ indentedSyntax: false, outputStyle: "compressed" }))
+        .pipe(gulp.dest(`./wwwroot/css`));
 }
+const transformJson = through2.obj((file, encoding, callback) => {
+    const content = file.contents.toString();
+    const json = JSON.stringify(JSON.parse(content).map(contributor => contributor.guildedId));
+
+    file.contents = Buffer.from(json);
+    file.path = file.path.replace(/contributors[.]json$/, "contributorIds.json");
+
+    callback(null, file);
+});
