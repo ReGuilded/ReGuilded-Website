7ba87bce95123d9c940bb436ff3c75a9ac02ffbb
Nathaniel M <nmoratto629@gmail.com>
2022-04-09 15:53:57 -0700

    Globalization/Localization addition to Website.
    - Core workings.

--- ReGuilded/Program.cs
+++ ReGuilded/Program.cs
@@ -2,8 +2,17 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddServerSideBlazor();
 builder.Services.AddRazorPages();
+builder.Services.AddLocalization();
+builder.Services.AddControllers();
 
 WebApplication app = builder.Build();
+
+var supportedCultures = new[] { "en-US" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+                                                          .AddSupportedCultures(supportedCultures)
+                                                          .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
 else app.UseExceptionHandler("/Error").UseHsts();
@@ -13,6 +22,7 @@ app
     .UseStaticFiles()
     .UseRouting();
 
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
