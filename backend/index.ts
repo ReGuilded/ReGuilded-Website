2cf3f39c8ef9a1e43a6baee0b6f9948997acc1a7
Atom <99760654+atomisadev@users.noreply.github.com>
2023-02-07 22:58:54 +0000

    gitignore fixed

--- backend/index.ts
+++
@@ -1,8 +0,0 @@
-import App from "./modules/App";
-import getAllRouter from "./controllers/team/getAll";
-
-const app = new App(3001);
-
-app.addControllers([{ path: "/team/getAll", router: getAllRouter }]);
-
-app.start();
--- utils/getProfilePicture.ts
+++
@@ -1,7 +0,0 @@
-export async function getProfilePicture(guildedId: string) {
-  const response = await fetch(
-    `https://guilded.gg/api/users/${guildedId}/profilev3`
-  );
-  const user = await response.json();
-  return user.profilePicture;
-}
