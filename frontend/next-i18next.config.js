55c67baffdc1f0d78f8032974af564ea08284387
Nathaniel M <me@itznxthaniel.dev>
2023-02-26 09:28:09 -0800

    Update supported langs

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     i18n: {
         defaultLocale: "en-US",
-        locales: ["en-US", "zh-CN"],
+        locales: ["en-US", "es-ES", "fr-FR", "zh-CN"],
     },
 
     localePath: typeof window === "undefined" ? require("path").resolve("./lang") : '/lang',
