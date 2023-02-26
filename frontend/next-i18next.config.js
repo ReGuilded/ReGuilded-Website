15d3cc240c5c69c4e8ee07bd4c5cef0be8e57784
Nathaniel M <me@itznxthaniel.dev>
2023-02-26 00:27:38 -0800

    Add Chinese Simplified to supported locales

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     i18n: {
         defaultLocale: "en-US",
-        locales: ["en-US"],
+        locales: ["en-US", "zh-CN"],
     },
 
     localePath: typeof window === "undefined" ? require("path").resolve("./lang") : '/lang',
