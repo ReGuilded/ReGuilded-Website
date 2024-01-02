5a4a9c7b5a8f5e944d6965bcfb50fdc935a29331
Nathaniel M <me@itznxthaniel.dev>
2024-01-02 12:58:30 -0500

    Add Hungarian

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -3,17 +3,18 @@
 module.exports = {
     i18n: {
         defaultLocale: "en-US",
-        locales: ["en-US", "es-ES", "fr-FR", "zh-CN", "de-DE"],
+        locales: ["en-US", "es-ES", "fr-FR", "zh-CN", "de-DE", "hu-HU"],
     },
     fallbackLng: {
         default: ["en"],
-        'es': ["es-ES"],
-        'fr': ["fr-FR"],
-        'zh': ["zh-CN"],
-        'de': ["de-DE"]
+        "es": ["es-ES"],
+        "fr": ["fr-FR"],
+        "zh": ["zh-CN"],
+        "de": ["de-DE"],
+        "hu": ["hu-HU"]
     },
     nonExplicitSupportedLngs: true,
 
-    localePath: typeof window === "undefined" ? require("path").resolve("./lang") : '/lang',
-    reloadOnPrerender: process.env.NODE_ENV === 'development'
+    localePath: typeof window === "undefined" ? require("path").resolve("./lang") : "/lang",
+    reloadOnPrerender: process.env.NODE_ENV === "development"
 }
